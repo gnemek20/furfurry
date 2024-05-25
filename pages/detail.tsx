a761e68ac8827b34f9d53e446e106b1fc5775515
gnemek20 <gnemek20@gmail.com>
2024-05-26 08:47:27 +0900

    errorCheckFur

---
+++ pages/detail.tsx
@@ -0,0 +1,90 @@
+import style from '@/styles/detail/Detail.module.css'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const detail = (serverSideProps: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+
+  const [a, sa] = useState<string>('');
+
+  useEffect(() => {
+    const posts = serverSideProps.posts;
+    
+    posts.map((post: typeof serverSideProps.posts[0]) => {
+      if (post.mimeType === 'image/jpeg') {
+        sa(`https://drive.google.com/uc?export=view&id=${post['id']}`)
+        const target: HTMLCanvasElement = document.getElementById('asd') as HTMLCanvasElement;
+        const context = target.getContext('2d');
+
+        let getImage = new Image();
+        // getImage.src = `https://drive.google.com/uc?export=view&id=${post['id']}`;
+        getImage.src = `https://drive.google.com/uc?export=view&id=1KKGsli_BanI1u35AZjFjA4azL-9YEg_g`;
+        // getImage.src = `https://drive.google.com/file/d/1KKGsli_BanI1u35AZjFjA4azL-9YEg_g/view?usp=drive_link`;
+        console.log(getImage)
+        getImage.onload = () => {
+          console.log('loaded')
+          context?.drawImage(getImage, 0, 0, target.width, target.height);
+        }
+      }
+    });
+  }, [serverSideProps]);
+
+  useEffect(() => {
+    const search = serverSideProps.search;
+
+    if (search) {
+      if (search === 'lost connection') {
+        window.alert('서버 오류');
+        router.reload();
+
+        return;
+      }
+    }
+  }, []);
+
+  return (
+    <div className={`flex justifyCenter`}>
+      <div className={`lockMaxWidth fullWidth fullHeight flex flexColumn`}>
+        <div className={`${style.titleContainer}`}>
+          <h1 className={`heading`}></h1>
+        </div>
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" />
+        <img src="https://drive.google.com/file/d/1KKGsli_BanI1u35AZjFjA4azL-9YEg_g/view" />
+        <canvas id='asd' />
+      </div>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const query = context.query['id'];
+  let search = query;
+  let result;
+  
+  const json = { postId: query }
+
+  const res = await fetch('http://localhost:3000/drive/getPost', {
+    mode: 'cors',
+    method: 'post',
+    body: JSON.stringify(json),
+    headers: { 'Content-Type': 'application/json' }
+  }).catch(() => { search = 'lost connection' });
+
+  if (res?.ok) {
+    try {
+      await res.json().then(parser => { result = parser });
+    } catch (Exception) {
+      search = 'lost connection';
+    }
+  }
+
+  return {
+    props: {
+      search: search,
+      posts: result || ''
+    }
+  }
+}
+
+export default detail;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import style from '@/styles/Home.module.css'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { ChangeEvent, useEffect } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 const clearIcon = {
   src: require('@/public/icons/close.svg'),
@@ -12,31 +12,57 @@ const clearIcon = {
 const home = (serverSideProps: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   let timeoutId: NodeJS.Timeout;
   const router = useRouter();
-
+  
+  const [posts, setPosts] = useState<Array<Object>>([]);
+  const [postCount, setPostCount] = useState<number>(0);
+  
   const clearSearchInputValue = () => {
     const target: HTMLInputElement = document.getElementById('searchInput') as HTMLInputElement;
-    target.value = '';
+    if (target.value === '') return;
 
+    target.value = '';
     router.push('', undefined, { shallow: false });
   }
-
+  
   const searchPost = (event: ChangeEvent<HTMLInputElement>) => {
-    clearInterval(timeoutId);
+    clearTimeout(timeoutId);
 
     timeoutId = setTimeout(() => {
-      const postName = event.target.value;
+      const postName = event.target.value.replace(/ /g, '');
 
       if (postName) router.push(`?search=${postName}`, undefined, { shallow: false });
       else router.push('', undefined, { shallow: false });
     }, 100);
   }
 
+  useEffect(() => {
+    const list = serverSideProps.list as Array<Object>;
+    setPosts(list);
+    setPostCount(list.length);
+  }, [serverSideProps])
+
+  useEffect(() => {
+    const search = serverSideProps['search'];
+
+    if (search) {
+      if (search === 'lost connection') {
+        window.alert('서버 오류');
+        router.reload();
+
+        return;
+      }
+
+      const target: HTMLInputElement = document.getElementById('searchInput') as HTMLInputElement;
+      target.value = serverSideProps.search;
+    }
+  }, []);
+
   return (
     <div className={`flex justifyCenter`}>
       <div className={`lockMaxWidth fullWidth fullHeight flex flexColumn`}>
         <div className={`${style.headerContainer}`}>
           <div className={`${style.headerRow} justifyCenter`}>
-            <h1 className={`header`}>퍼퍼리 갤러리</h1>
+            <h1 className={`heading`}>퍼퍼리 갤러리</h1>
           </div>
         </div>
         <div className={`${style.bodyContainer}`}>
@@ -47,18 +73,50 @@ const home = (serverSideProps: InferGetServerSidePropsType<typeof getServerSideP
             </div>
             <button className={`${style.addPostButton} text`}>게시글 추가</button>
           </div>
+          <div className={`${style.bodyRow} ${style.countText}`}>
+            <p className={`description`}>{ postCount }개</p>
+          </div>
+          <div className={`${style.bodyRow} ${style.posts}`}>
+            {
+              posts.map((post: { name?: string, id?: string }, index) => (
+                <div className={`${style.post}`} onClick={() => router.push(`/detail?id=${post['id']}`)} key={index}>
+                  <p className={`text`}>{ post['name'] }</p>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const query = context.query['search'] || '';
+  const query: string = context.query['search'] as string || '';
+  let search = query;
+  let result;
+
+  const json = { postName: query }
+
+  const res = await fetch('http://localhost:3000/drive/search', {
+    mode: 'cors',
+    method: 'post',
+    body: JSON.stringify(json),
+    headers: { 'Content-Type': 'application/json' }
+  }).catch(() => { search = 'lost connection' });
+
+  if (res?.ok) {
+    try {
+      await res.json().then(parser => { result = parser });
+    } catch (Exception) {
+      search = 'lost connection';
+    }
+  }
 
   return {
     props: {
-      search: query
+      search: search,
+      list: result || []
     }
   }
 }
